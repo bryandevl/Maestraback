6677a8389cafa28a6eba50c6396b3517b93e59cd
bryandevl <bryan.henry@outlook.es>
2025-03-27 14:45:43 -0500

    actualizacion bryan

--- src/app.module.ts
+++ src/app.module.ts
@@ -47,13 +47,27 @@ import {ExcelController} from './controllers/cargabases.controller';
 
 import { BqCourier } from './entities/BQCourier.entity';
 import { BQCourierService } from './services/BQCourrier.service';
+import { OmnicanalDatabaseModule } from './database/omnicanal-database.module';
 
 
-
-
+import { Pass1Service } from './services/Omni1.service';
+import { Pass2Service   } from './services/Omni2.service';
+import {Pass3Service} from './services/Omni3.service';
+import { PassController } from './controllers/omni.controller';
+import { OmnicanalDatabaseModule2 } from './database/omnicanal2-database.module';
+import { BlasterDatabaseModule } from './database/blaster-database.module';
+import { BlasterService } from './services/blaster.service';
+import { OmnicanalDatabaseModule3 } from './database/omnicanal3-database.module';
+import { EncryptionController } from './controllers/pass.controller';
+import { EncryptionService } from './services/pass.service';
+import { ZimbraService  } from 'src/services/zimbra.service';
+import {AsteriskdService} from 'src/services/asterisk.service';
+import { InfocallService } from './services/infocall.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
+    HttpModule,
     TypeOrmModule.forFeature([Campaign,MainClientes,PagosEntity,FotogestionesEntity,ObtenerWspmasivoClt,ObtenerSmsMasivoClt,DatosAdicionalesCltEntity,DetalleDeudaCltEntity,ObtenerIvrMasivoClt,BqCourier,ColumnaMask,UpdateColumnMask,ColumnasMask2,MascaraFormato]), // Especificamos la conexión 'primary'
     ConfigModule.forRoot({
       // * Definimos que es global
@@ -65,8 +79,13 @@ import { BQCourierService } from './services/BQCourrier.service';
       validationSchema: Joi.object(validation),
     }),
     DatabaseModule,
+    OmnicanalDatabaseModule,
+    OmnicanalDatabaseModule2,
+    OmnicanalDatabaseModule3,
+    BlasterDatabaseModule,
+    
   ],
-  providers: [CampaignService,MainClientService,PagosService,GestionService,ExcelService,ObtenerWspmasivoCltService,ObtenerSmsMasivoCltService,DatosAdicionalCltService,ObtenerIvrMasivoCltService,DetalleDeudaCltService,FrPagosService,MascaraService,BQCourierService,ColumnaMaskService,UpdateColumnMaskService,ColumnasMaskService2,VrcResultMaskService,clearMascaraService,MascaraFormatoService],
-  controllers: [AppController, MaestraController,ExcelController],
+  providers: [CampaignService,MainClientService,PagosService,GestionService,ExcelService,ObtenerWspmasivoCltService,ObtenerSmsMasivoCltService,DatosAdicionalCltService,ObtenerIvrMasivoCltService,DetalleDeudaCltService,FrPagosService,MascaraService,BQCourierService,ColumnaMaskService,UpdateColumnMaskService,ColumnasMaskService2,VrcResultMaskService,clearMascaraService,MascaraFormatoService,Pass1Service,Pass2Service,BlasterService,Pass3Service,EncryptionService,ZimbraService,AsteriskdService,InfocallService],
+  controllers: [AppController, MaestraController,ExcelController,PassController,EncryptionController],
 })
 export class AppModule {}
