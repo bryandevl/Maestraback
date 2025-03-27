9bfc158924b520d5fa331e6ff7ba1250f4a1e210
bryandevl <bryan.henry@outlook.es>
2025-03-27 14:49:00 -0500

    actualizacion bryan

---
+++ src/database/omnicanal-database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioOmnicanal } from 'src/entities/userOmni.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+        name: 'OMNICANAL',
+        type: 'mssql',
+        host: '192.168.21.15',
+        port: 1433,
+        username: 'PowerBi',
+        password: 'Aa123456',
+        database: 'BD_OMNICANAL',
+        options: {
+          encrypt: false,
+          trustServerCertificate: true,
+        },
+        autoLoadEntities: true, //  Cambiar a true
+        synchronize: false,
+        logging: false,
+      }), // Conexión a la base de datos
+    TypeOrmModule.forFeature([UsuarioOmnicanal], 'OMNICANAL'), // Registra la entidad
+  ],
+  exports: [TypeOrmModule], // Exportamos para que otros módulos lo usen
+})
+export class OmnicanalDatabaseModule {}
