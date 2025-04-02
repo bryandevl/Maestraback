a1791356cc347437d037f62a90ce1f9b34a0aec8
bryandevl <bryan.henry@outlook.es>
2025-04-02 11:03:44 -0500

    acutalizacion bryan

--- src/app.module.ts
+++ src/app.module.ts
@@ -48,22 +48,12 @@ import {ExcelController} from './controllers/cargabases.controller';
 
 import { BqCourier } from 'src/entities/BqCourier.entity';
 import { BQCourierService } from './services/BQCourrier.service';
-import { OmnicanalDatabaseModule } from './database/omnicanal-database.module';
 
 
-import { Pass1Service } from './services/Omni1.service';
-import { Pass2Service   } from './services/Omni2.service';
-import {Pass3Service} from './services/Omni3.service';
-import { PassController } from './controllers/omni.controller';
-import { OmnicanalDatabaseModule2 } from './database/omnicanal2-database.module';
-import { BlasterDatabaseModule } from './database/blaster-database.module';
-import { BlasterService } from './services/blaster.service';
-import { OmnicanalDatabaseModule3 } from './database/omnicanal3-database.module';
-import { EncryptionController } from './controllers/pass.controller';
 import { EncryptionService } from './services/pass.service';
-import { ZimbraService  } from 'src/services/zimbra.service';
+
 import {AsteriskdService} from 'src/services/asterisk.service';
-import { InfocallService } from './services/infocall.service';
+
 import { HttpModule } from '@nestjs/axios';
 import {UserLogService} from './services/user-log.service'; 
 import {UserLog} from './entities/user-log.entity'; // Asegúrate de que la ruta sea correcta
@@ -86,13 +76,10 @@ import { vrcResultadoClienteService } from './services/vrc-resultadocliente.serv
       validationSchema: Joi.object(validation),
     }),
     DatabaseModule,
-    OmnicanalDatabaseModule,
-    OmnicanalDatabaseModule2,
-    OmnicanalDatabaseModule3,
-    BlasterDatabaseModule,
+ 
     
   ],
-  providers: [CampaignService,MainClientService,PagosService,GestionService,ExcelService,ObtenerWspmasivoCltService,ObtenerSmsMasivoCltService,DatosAdicionalCltService,ObtenerIvrMasivoCltService,DetalleDeudaCltService,FrPagosService,MascaraService,BQCourierService,ColumnaMaskService,UpdateColumnMaskService,ColumnasMaskService2,VrcResultMaskService,clearMascaraService,MascaraFormatoService,Pass1Service,Pass2Service,BlasterService,Pass3Service,EncryptionService,ZimbraService,AsteriskdService,InfocallService,UserLogService,vrcResultadoClienteService],
-  controllers: [AppController, MaestraController,ExcelController,PassController,EncryptionController],
+  providers: [CampaignService,MainClientService,PagosService,GestionService,ExcelService,ObtenerWspmasivoCltService,ObtenerSmsMasivoCltService,DatosAdicionalCltService,ObtenerIvrMasivoCltService,DetalleDeudaCltService,FrPagosService,MascaraService,BQCourierService,ColumnaMaskService,UpdateColumnMaskService,ColumnasMaskService2,VrcResultMaskService,clearMascaraService,MascaraFormatoService,EncryptionService,AsteriskdService,UserLogService,vrcResultadoClienteService],
+  controllers: [AppController, MaestraController,ExcelController],
 })
 export class AppModule {}
